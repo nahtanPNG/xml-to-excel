d04e6b1bc659b10136c683dd3ae52ef663aff520
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-04-13 21:02:19 -0300

    TODO: Instruções

---
+++ billing_report.py
@@ -0,0 +1,4 @@
+# TODO:
+# Ler todas as notas fiscais de uma pasta
+# Identificar notas de entrada/devolução e saída -> (1 ou 2 - entrada) (5 ou 6 - saida)
+# Retornar o valor da nota de saida - valores das notas de devolução
